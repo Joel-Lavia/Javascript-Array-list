00dee3b507a9ba1e6db93bf3f89e81a2f4c81002
JoelD-418 <joeldisidi8@gmail.com>
2023-08-25 02:05:33 +0100

    Array-liste-complet

--- index.js
+++ index.js
@@ -5,51 +5,22 @@ console.log = function() {
   console.logs.push(Array.from(arguments).join(" "));
   console.stdlog.apply(console, arguments);
 }
-
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
-
-// Your code here 
-//1. on supprime le dernier élément dans le tableau
+//1.Use an array method to remove the last string of the array secretMessage.
 secretMessage.pop();
-console.log(secretMessage);
-//2. on affiche le nombre d'élement dans le tableau
+//2.Great! You can check your work by logging the .length of the array. At this point, the length should be 1 less than the original length.
 console.log(secretMessage.length);
-//3. On ajoute deux mots to et program à la fin du tableau 
+//3.Use an array method to add the words to and Program as separate strings to the end of the secretMessage array.
+
 secretMessage.push('to', 'Program');
-console.log(secretMessage);
-//4.on supprime la première chaine de caractère dans le tableau
+//4.Change the word easily to the word right by accessing the index and replacing it.
+//5.Use an array method to remove the first string of the array.
 secretMessage.shift();
-console.log(secretMessage);
-//5.
-let index = 7;
-let newWord = 'right';
-secretMessage[7] = newWord;
-console.log(secretMessage);
-//6. on ajoute une chaine de caractère au debut du tableau
+
+secretMessage.splice(7, 1, 'right');
+//6.Use an array method to add the string Programming to the beginning of the array.
 secretMessage.unshift('Programming');
-console.log(secretMessage);
-
-
-
-//7.ajout d'une chaine au debut du 
-let index1 = [6, 7, 8, 9, 10];
-let newWord1 = 'know';
-secretMessage = secretMessage.filter(item => !index1.includes(item));
-secretMessage[6, 7, 8, 9, 10] = newWord1;
-console.log(secretMessage);
-
-
-
-//8.
-let phrase = secretMessage.join(' ');
-console.log(phrase);
-const newelement = ['Je suis un dévévoloppeur', 'qui a dévélopper', 'plusieurs site web.'];
-// jethroMuluba.shift();
-// console.log(jethroMuluba.shift());
-// jethroMuluba = ['Je suis un dévévoloppeur', 'qui a dévélopper', 'plusieurs site web.'];
-console.log(newelement);
-
-
-
-// Don't remove this line 
-module.exports = { secretMessage }
+//7.Use an array method to remove the strings get, right, the, first, time,, and replace them with the single string know,.
+secretMessage.splice(6, 5, 'know');
+//8. On one line, use console.log() and .join() to print the secret message as a sentence.
+console.log(secretMessage.join(' '));
